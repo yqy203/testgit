99b48868d43ab447f0541a0df9e4b891fc3be401
yqy203 <2383960336@qq.com>
2020-04-03 23:21:46 +0800

    第一次提交

---
+++ src/test/java/Demo.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+/**
+ * @Author: yeqy
+ * @Description:
+ * @Date: Create in 23:07 2020/4/3
+ */
+public class Demo {
+    @Test
+    public void test(){
+        System.out.println("test======================================");
+    }
+}
